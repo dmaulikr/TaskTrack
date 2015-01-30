9dcb8e131e1f301a3391d56cd4628e53b2629a17
mdlinington <mdlinington@gmail.com>
2015-01-30 04:34:01 -0500

    Initial Commit

---
+++ TaskTrack/TaskTrack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaskTrack
+//
+//  Created by Mark Linington on 1/30/15.
+//  Copyright (c) 2015 MarkLinington. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TaskTrack/TaskTrack/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  TaskTrack
+//
+//  Created by Mark Linington on 1/30/15.
+//  Copyright (c) 2015 MarkLinington. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController <UIPageViewControllerDelegate>
+
+@property (strong, nonatomic) UIPageViewController *pageViewController;
+
+@end
+
